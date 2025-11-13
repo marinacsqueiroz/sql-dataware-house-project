5d3cd810ae137913df79d4bab25b72af53db99c5
Marina Queiroz <marina.queiroz@grupoorion.com.br>
2025-11-13 18:16:15 +0100

    ✨ counting time to process

--- scripts/start_project/main.py
+++ scripts/start_project/main.py
@@ -21,6 +21,7 @@
 # CRITICAL ERRORS with full traceback (exc_info=True), preventing silent failures.
 # -----------------------------------------------------------------------------
 import os
+import time
 from create_schema_and_models import create_schema_and_models
 from data_profile import analyse_dataset
 from log import LogManager
@@ -35,37 +36,47 @@ datasets_file_path = os.path.join(base_path)
 log_manager = LogManager(subdirectory='start_project')
 logger = log_manager.get_logger()
 
-# logger.info("--- STARTING MAIN PROJECT CREATION AND ANALYSIS PROCESS ---")
-
-# logger.info(f"Starting creation of schemas and models from: {initial_project_file_path}")
-# try:
-#     result_schema_and_models = create_schema_and_models(initial_project_file_path, logger=logger)
+logger.info("--- STARTING MAIN PROJECT CREATION AND ANALYSIS PROCESS ---")
+start_time_all_process = time.time()
+logger.info(f"Starting creation of schemas and models from: {initial_project_file_path}")
+try:
+    start_time = time.time()
+    result_schema_and_models = create_schema_and_models(initial_project_file_path, logger=logger)
     
-#     if result_schema_and_models:
-#         logger.info("Schema and model creation successfully completed.")
-#     else:
-#         logger.warning("Schema and model creation completed, but returned a neutral/false status.")
+    if result_schema_and_models:
+        end_time = time.time()
+        duration = end_time-start_time
+        logger.info(f"Schema and model creation successfully completed in {duration:.4f} seconds.")
+    
+    else:
+        logger.warning("Schema and model creation completed, but returned a neutral/false status.")
 
-# except Exception as e:
-#     logger.error(f"CRITICAL ERROR during schema and model creation: {e}", exc_info=True)
+except Exception as e:
+    logger.error(f"CRITICAL ERROR during schema and model creation: {e}", exc_info=True)
 
 logger.info(f"Starting dataset analysis (profiling) in: {base_path}")
 try:
+    start_time = time.time()
     result_analyse_dataset = analyse_dataset(base_path, create_table_config_file_path, logger=logger, output_path=data_config_file_path)
-    
-    logger.info(f"Dataset analysis completed. Configurations saved to: {data_config_file_path}")
+    end_time = time.time()
+    duration = end_time-start_time
+    logger.info(f"Dataset analysis completed. Configurations saved to: {data_config_file_path} in {duration:.4f} seconds")
     
 except Exception as e:
     logger.error(f"CRITICAL ERROR during dataset analysis and configuration generation: {e}", exc_info=True)
 
-# logger.info(f"Starting table creation using config: {data_config_file_path}")
-# try:
-#     result_create_table = create_table(data_config_file_path, datasets_file_path, logger=logger)
-    
-#     logger.info("Table (or DDL/DML model) creation successfully completed.")
+logger.info(f"Starting table creation using config: {data_config_file_path}")
+try:
+    start_time = time.time()
+    result_create_table = create_table(data_config_file_path, datasets_file_path, logger=logger)
+    end_time = time.time()
+    duration = end_time-start_time
+    logger.info(f"Table (or DDL/DML model) creation successfully completed in {duration:.4f} seconds.")
 
-# except Exception as e:
-#     logger.error(f"CRITICAL ERROR during table creation: {e}", exc_info=True)
+except Exception as e:
+    logger.error(f"CRITICAL ERROR during table creation: {e}", exc_info=True)
 
-logger.info("--- MAIN PROCESS CONCLUDED ---")
+end_time_all_process = time.time()
+duration_all_process = end_time_all_process-start_time_all_process
+logger.info(f"--- MAIN PROCESS CONCLUDED IN {duration_all_process:.4f} SECONDS---")
 log_manager.flush_and_close()
